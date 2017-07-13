9ece7ed708a61002dfc1f337ce593b3a51822af5
leo <leo@proteus-tech.com>
2017-07-13 15:55:38 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,60 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.showUser=this.showUser.bind(this)
+    this.state = {
+      show: [this.props.users.length],
+      a: "nohighlight"
+    }
+  }
+  handleClick(a){
+    console.log("hi")
+  }
+  showUser(name){
+    if(this.state.show[this.props.users.indexOf(name)]===false)
+    {
+      this.state.show[this.props.users.indexOf(name)]=true
+      this.setState({
+        a: "nohighlight"
+      })
+    } else {
+      this.state.show[this.props.users.indexOf(name)]=false
+      this.setState({
+        a: "highlight"
+      })
+    }
+    console.log(this.state.show[this.props.users.indexOf(name)])
+  }
+  getColor(name){
+    if(this.state.show[this.props.users.indexOf(name)]){
+      return "nohighlight"
+    } else {
+      return "highlight"
+    }
+  }
   render() {
+    console.log(this.state.a)
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div id="data">
+        <div id="names">
+          {this.props.users.map((user) => (
+            <div id="username" onClick={this.showUser}>
+              {user.name}
+            </div>
+          ))}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table>
+        {this.props.users.map((user) => (
+          <tbody id={this.state.a}>
+              <td>{user.name}</td>
+              <td>Category</td>
+              <td>Brand</td>
+              <td>Model</td>
+              <td>Serial Number</td>
+            </tbody>))}
+        </table>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+//import registerServiceWorker from './regiserServiceWorker';
+const users = [
+  {name: 'A'},
+  {name: 'B'},
+  {name: 'C'},
+  {name: 'D'}
+]
+ReactDOM.render(<App users={users} />, document.getElementById('root'));
 //registerServiceWorker();

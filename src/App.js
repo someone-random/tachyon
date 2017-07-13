8dd01f4b343923c186f2b90a3eba7e84eabe2ce7
leo <leo@proteus-tech.com>
2017-07-13 17:19:41 +0700

    setState but doesn't change value

--- src/App.js
+++ src/App.js
@@ -6,28 +6,39 @@ class App extends Component {
   constructor(props){
     super(props);
     this.showUser=this.showUser.bind(this)
-    this.state = {
+    this.state = ({
       show: [this.props.users.length],
-      a: "nohighlight"
-    }
+      a: [this.props.users.length]
+    })
+  for (var i=0;i<this.props.users.length;i++){
+    this.setState({
+        [this.state.show[i]]: true,
+        [this.state.a[i]]: "nohighlight",
+    })
+    console.log(this.state.show)
   }
+}
   showUser(name){
     if(this.state.show[this.props.users.indexOf(name)]===false)
     {
-      this.state.show[this.props.users.indexOf(name)]=true
       this.setState({
-        a: "nohighlight"
+        [this.state.show[this.props.users.indexOf(name)]]: true,
+        [this.state.a[this.props.users.indexOf(name)]]: "nohighlight",
       })
     } else {
-      this.state.show[this.props.users.indexOf(name)]=false
       this.setState({
-        a: "highlight"
+        [this.state.show[this.props.users.indexOf(name)]]: false,
+        [this.state.a[this.props.users.indexOf(name)]]: "highlight",
       })
     }
     console.log(this.state.show[0])
     console.log(this.state.show[1])
     console.log(this.state.show[2])
     console.log(this.state.show[3])
+    console.log(this.state.a[0])
+    console.log(this.state.a[1])
+    console.log(this.state.a[2])
+    console.log(this.state.a[3])
   }
   getColor(name){
     if(this.state.show[this.props.users.indexOf(name)]){
@@ -48,7 +59,7 @@ class App extends Component {
         </div>
         <table>
         {this.props.users.map((user) => (
-          <tbody id={this.state.a}>
+          <tbody className={this.state.a[this.props.users.indexOf(user)]}>
               <td>{user.name}</td>
               <td>Category</td>
               <td>Brand</td>

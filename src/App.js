aa657ade1990705e8cb0b69219a91b2ca26ab19f
Leo <leo@atom.local>
2017-07-17 20:32:07 +0700

    login logout working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import * as firebase from 'firebase'
 class App extends Component {
   constructor(props){
     super(props);
     this.showUser=this.showUser.bind(this)
     this.state = ({
-      show: [this.props.users.length],
+      show: [this.props.users.length], 
       a: [this.props.users.length],
-      b:[this.props.users.length]
+      b:[this.props.users.length],
+      email: ""
     })
     for (var i=0;i<this.props.users.length;i++){
-      this.state.show[i]= true,
+      this.state.show[i]= true
       this.state.a[i]= "nohighlight"
       this.state.b[i]="username"
       }
     }
+  login(){
+    firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider())
+    firebase.auth().getRedirectResult().then(function(result){
+      if(result.credential){
+        var token = result.credential.accessToken
+        console.log("hi")
+      }
+      var user = result.user
+    }).catch(function(error) {
+        console.log("error")
+    })
+  }
 
-
+  logout(){
+    firebase.auth().signOut().then(function() {
+      console.log("signed out")
+    }).catch(function(error) {
+      console.log("error")
+    })
+  }
   showUser(name){
     if(this.state.show[this.props.users.indexOf(name)]===false)
     {
@@ -34,18 +53,29 @@ class App extends Component {
   }
 
   render() {
+    firebase.auth().onAuthStateChanged(function(user){
+          if(user){
+            this.state.email = user
+            console.log(this.state.email.email)
+          }
+          else{
+            console.log("out")
+          } 
+        })
     return (
       <div id="data">
+        <button type="button" onClick={() => this.login()}>Sign in</button>
+        <button type="button" onClick={() => this.logout()}>Sign out</button>
         <div id="names">
           {this.props.users.map((user) => (
-            <div className={this.state.b[this.props.users.indexOf(user)]} onClick={() => this.showUser(user)}>
+            <div className={this.state.b[this.props.users.indexOf(user)]} onClick={() => this.showUser(user)} key={this.props.users.indexOf(user)}>
               {user.name}
             </div>
           ))}
         </div>
         <table>
           {this.props.users.map((user) => (
-            <tbody className={this.state.a[this.props.users.indexOf(user)]}>
+            <tbody className={this.state.a[this.props.users.indexOf(user)]} key={this.props.users.indexOf(user)}>
               <tr>
                 <td>{user.name}</td>
                 <td>Category</td>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as firebase from 'firebase'
 //import registerServiceWorker from './regiserServiceWorker';
 const users = [
   {name: 'A'},
@@ -14,5 +15,14 @@ const users = [
   {name: 'H'},
   {name: 'I'}
 ]
+var config = {
+  apiKey: "AIzaSyAj0Q6DBp0d6xXd3qlts4c0q-ZWzRzP8lI",
+  authDomain: "tachyon-173612.firebaseapp.com",
+  databaseURL: "https://tachyon-173612.firebaseio.com",
+  projectId: "tachyon-173612",
+  storageBucket: "tachyon-173612.appspot.com",
+  messagingSenderId: "336062402100"
+};
+firebase.initializeApp(config);
 ReactDOM.render(<App users={users} />, document.getElementById('root'));
 //registerServiceWorker();
